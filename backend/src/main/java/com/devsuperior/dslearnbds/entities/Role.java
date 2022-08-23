e95e869b7c9555de35e71db6836f0675574fea0f
Jota-Erre-JR <juliocesarjr@outlook.com>
2022-08-23 19:31:26 -0300

    User Role

---
+++ backend/src/main/java/com/devsuperior/dslearnbds/entities/Role.java
@@ -0,0 +1,50 @@
+package com.devsuperior.dslearnbds.entities;
+
+import java.io.Serializable;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "tb_role")
+public class Role implements Serializable{
+	private static final long serialVersionUID = 1L;
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	
+	private String authority;
+	
+	
+
+	public Role() {
+	}
+
+	public Role(Long id, String authority) {
+		this.id = id;
+		this.authority = authority;
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getAuthority() {
+		return authority;
+	}
+
+	public void setAuthority(String authority) {
+		this.authority = authority;
+	}
+	
+	
+
+}
