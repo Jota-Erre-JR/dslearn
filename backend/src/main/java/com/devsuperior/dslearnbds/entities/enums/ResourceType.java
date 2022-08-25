31c2d003c9fb649af39f1d4fe76324af7cd803d8
Jota-Erre-JR <juliocesarjr@outlook.com>
2022-08-25 16:56:22 -0300

    Resource

---
+++ backend/src/main/java/com/devsuperior/dslearnbds/entities/enums/ResourceType.java
@@ -0,0 +1,9 @@
+package com.devsuperior.dslearnbds.entities.enums;
+
+public enum ResourceType {
+	
+	LESSON_ONLE,
+	lESSON_TASK,
+	FORUM,
+	EXTERNAL_LINK;
+}
--- backend/src/main/resources/import.sql
+++ backend/src/main/resources/import.sql
@@ -15,5 +15,9 @@ INSERT INTO tb_user_role (user_id, role_id) VALUES (3, 3);
 
 INSERT INTO tb_course (name, img_Uri, img_Gray_Uri) VALUES ('Bootcamp Web Developer', 'C:\ws-bootcampds\dslearn\assets\img\full-stack-web-developer.png', 'C:\ws-bootcampds\dslearn\assets\img\full-stack-web-developer-gray.png')
 
-INSERT INTO tb_offer (edition, start_Moment, end_Moment, course_id) VALUES ('1.0', TIMESTAMP WITH TIME ZONE '2020-11-20T00:00:00', TIMESTAMP WITH TIME ZONE '2021-11-20T00:00:00', 1)
-INSERT INTO tb_offer (edition, start_Moment, end_Moment, course_id) VALUES ('2.0', TIMESTAMP WITH TIME ZONE '2021-11-20T00:00:00', TIMESTAMP WITH TIME ZONE '2022-11-20T00:00:00', 1)
+INSERT INTO tb_offer (edition, start_Moment, end_Moment, course_id) VALUES ('1.0', TIMESTAMP WITH TIME ZONE '2020-11-20T00:00:00', TIMESTAMP WITH TIME ZONE '2021-11-20T00:00:00', 1);
+INSERT INTO tb_offer (edition, start_Moment, end_Moment, course_id) VALUES ('2.0', TIMESTAMP WITH TIME ZONE '2021-11-20T00:00:00', TIMESTAMP WITH TIME ZONE '2022-11-20T00:00:00', 1);
+
+INSERT INTO tb_resource (title, description, position, img_Uri, type, offer_id) VALUES ('Trilha WEB', 'Trilha Principal do Curso', 1, '"C:\ws-bootcampds\dslearn\assets\img\rocket.png"', 1, 1);
+INSERT INTO tb_resource (title, description, position, img_Uri, type, offer_id) VALUES ('Fórum', 'Tire suas dúvidas.', 2, '"C:\ws-bootcampds\dslearn\assets\img\suport.png"', 2, 1);
+INSERT INTO tb_resource (title, description, position, img_Uri, type, offer_id) VALUES ('Lives', 'Lives Exclusivas', 3, '"C:\ws-bootcampds\dslearn\assets\img\community.png"', 0, 1);
