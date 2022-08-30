606a2f07a2ef6f5c3a4ad04a6d1ef290fe23a820
Jota-Erre-JR <juliocesarjr@outlook.com>
2022-08-30 17:19:28 -0300

    Deliver

--- backend/src/main/resources/import.sql
+++ backend/src/main/resources/import.sql
@@ -49,3 +49,5 @@ INSERT INTO tb_notification (text, moment, read, route, user_id) VALUES ('Primei
 INSERT INTO tb_notification (text, moment, read, route, user_id) VALUES ('Segundo feedback: favor revisar', TIMESTAMP WITH TIME ZONE '2020-12-12T13:00:00Z', true, '/offers/1/resource/1/sections/1', 1);
 INSERT INTO tb_notification (text, moment, read, route, user_id) VALUES ('Terceiro feedback: favor revisar', TIMESTAMP WITH TIME ZONE '2020-12-14T13:00:00Z', true, '/offers/1/resource/1/sections/1', 1);
 
+INSERT INTO tb_deliver (uri, moment, status, feedback, correct_Count, lesson_id, user_id, offer_id) VALUES ('https://github.com/devsuperior/bds-dslearn', TIMESTAMP WITH TIME ZONE '2020-12-10T10:00:00Z', 0, null, null, 4, 1, 1);
+
