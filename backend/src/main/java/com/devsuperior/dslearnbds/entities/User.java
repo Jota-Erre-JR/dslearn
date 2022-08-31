a6df73b9bd45b167c7b6e24c37e57602bcfb9148
Jota-Erre-JR <juliocesarjr@outlook.com>
2022-08-31 17:33:16 -0300

    Fix User constructor

--- backend/src/main/java/com/devsuperior/dslearnbds/entities/User.java
+++ backend/src/main/java/com/devsuperior/dslearnbds/entities/User.java
@@ -45,6 +45,15 @@ public class User implements Serializable{
 
 	public User() {
 	}
+	
+	public User(Long id, String name, String email, String password) {
+		this.id = id;
+		this.name = name;
+		this.email = email;
+		this.password = password;
+	}
+
+
 
 	public Long getId() {
 		return id;
